e6364585ed6136c59930f658214d5a16c1138e36
Preetha <preethasurej20@gmail.com>
2023-10-04 16:02:16 +0530

    changes in mongodb

--- LoomCraft/config/mongoDb.js
+++ LoomCraft/config/mongoDb.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 const { db } = require('../models/orders');
 
-mongoose.connect("mongodb://127.0.0.1:27017/ecommerce")
+mongoose.connect("mongodb+srv://preethasurej20:UIOomNlScKJGQSL0@cluster0.0mcrzw0.mongodb.net/?retryWrites=true&w=majority")
 .then(()=>{
   console.log("Mongodb Connected");
 }).catch(()=>{
